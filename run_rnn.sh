d4469966fc57584587f497e65c55019d6e540d8b
Anthony D'Amato <anthony.damato@hotmail.fr>
2018-01-19 21:54:07 +0900

    update for server

--- run_rnn.sh
+++ run_rnn.sh
@@ -7,4 +7,4 @@
 
 . /fefs/opt/dgx/env_set/common_env_set.sh
 
-python rnn_planted.py --data 
+python rnn_planted.py --data clique-N1000-K25-E10-M1-exFalse-L:False-F:False
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Deep Planted Tree',
+      version='1.0',
+      description='Planted Tree Classification',
+      author='Anthony D\'Amato',
+      author_email='anthony.damato@hotmail.fr',
+      url='',
+      packages=['numpy', 'panda', 'h5py', 'panda', 'matplotlib', ],
+     )
