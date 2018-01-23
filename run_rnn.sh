a1823cccf0fa5f2fd624cebfba6902490a32a9a3
Anthony D'Amato <anthony.damato@hotmail.fr>
2018-01-23 11:24:04 +0900

    proxy correction

--- run_rnn.sh
+++ run_rnn.sh
@@ -5,6 +5,16 @@
 #$ -e err_rnn.o
 #$ -o std_rnn.o
 
+export MY_PROXY_URL="http://10.1.10.1:8080/"
+export HTTP_PROXY=$MY_PROXY_URL
+export HTTPS_PROXY=$MY_PROXY_URL
+export FTP_PROXY=$MY_PROXY_URL
+export http_proxy=$MY_PROXY_URL 
+export https_proxy=$MY_PROXY_URL
+export ftp_proxy=$MY_PROXY_URL
+
+
+
 . /fefs/opt/dgx/env_set/common_env_set.sh
 
 pip install -r requirements.txt
